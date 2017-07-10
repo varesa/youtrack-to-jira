b1f820be20ebf77db311d37604663999e5f1694c
Esa Varemo <esa@kuivanto.fi>
2017-07-10 21:15:38 +0300

    Managed to transfer the first issue (incomplete)

--- jira.py
+++ jira.py
@@ -1,12 +1,64 @@
+import base64
+import requests
+import sys
+
 import config
+from jirameta import populate_meta
+from mapping import map_issue_type
 
 
 class Jira:
 
     url = ""
+    headers = {}
+
+    meta = {}
 
     def __init__(self):
         self.url = config.JIRA_URL
 
     def connect(self, user, password):
-        return self
+        self.headers = {'Authorization': "Basic " + base64.b64encode((user + ":" + password).encode()).decode()}
+        r = requests.get(self.url + "/rest/auth/1/session", headers=self.headers)
+        if r.status_code == 200:
+            return self
+        else:
+            raise Exception("Error logging in: {}".format(r.status_code))
+
+    def does_project_exist(self, id):
+        r = requests.get(self.url + "/rest/api/2/project/" + id, headers=self.headers)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+
+    def get_field_id(self, name):
+        return self.meta['fields'][name]['id']
+
+    def get_issuetype_id(self, project, name):
+        return self.meta[project.id]['issuetypes'][name]['id']
+
+    def create_issue(self, project, issue):
+        print(issue.__dict__)
+        data = {
+            "fields": {
+                "project": {
+                    "key": project.id
+                },
+                "issuetype": {
+                    "name": map_issue_type(issue.Type)
+                },
+                "summary": issue.summary,
+                "description": issue.description
+
+            }
+        }
+        r = requests.post(self.url + "/rest/api/2/issue", json=data, headers=self.headers)
+        if r.status_code != 201:
+            raise Exception("Error creating issue: {} - {}".format(r.status_code, r.text))
+
+    def sync_project(self, project):
+        populate_meta(self, project)
+        #for issue in project.issues:
+        issue = project.issues[0]
+        self.create_issue(project, issue)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from jira import Jira
 from youtrack import Youtrack
 
@@ -9,6 +11,47 @@ youtrack = Youtrack().connect(username, password)
 jira = Jira().connect(username, password)
 
 projects = youtrack.get_projects()
-print(projects)
 
+states = {}
+types = {}
+priorities = {}
+resolutions = {}
+
+
+def increment(d, key):
+    if key not in d.keys():
+        d[key] = 1
+    else:
+        d[key] += 1
+
+for project in projects:
+    if jira.does_project_exist(project.id):
+        print("Project found in JIRA: " + project.id)
+        if project.id != "ALRK":
+            continue
+        jira.sync_project(project)
+
+    else:
+        print("Project not found in JIRA: " + project.id)
+
+"""if True:
+    for project in projects:
+        for issue in project.issues:
+            try:
+                increment(states, issue.State)
+                increment(types, issue.Type)
+                increment(priorities, issue.Priority)
+                if "Resolution" in issue.__dict__:
+                    increment(resolutions, issue.Resolution)
+
+                else:
+                    increment(resolutions, "Not set")
+            except:
+                print(issue)
+
+print("Things:")
+print(states)
+print(types)
+print(priorities)
+print(resolutions)"""
 
